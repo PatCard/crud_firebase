480e059995393d5beea379b9ad25b188b01e3b4b
PatCard <89741961+PatCard@users.noreply.github.com>
2023-05-30 00:39:45 -0400

    App se conecta a la base de datos Firebase y se controla el microsengundo donde el snapshot es Null

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,66 @@
+import 'package:crud_firebase/services/firebase_service.dart';
 import 'package:flutter/material.dart';
 
 // Import the generated file
-import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 
 void main() async {
   // Inicializar Firebase
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform
+  );
   // Ejecutar la aplicación
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({
+    Key? key,
+}) : super(key: key);
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome to Flutter'),
       ),
+      body: FutureBuilder(
+          future: getPeople(),
+          builder: ((context, snapshot){
+            if (snapshot.hasData){
+              return ListView.builder(
+                itemCount: snapshot.data?.length,
+                itemBuilder: (context, index) {
+                  return Text(snapshot.data?[index]['name']);
+                },
+              );
+            }else{
+              return const Center(
+                //child: CircularProgressIndicator(),
+                child: Text("Cargando....."),
+              );
+            }
+          })),
     );
   }
 }
--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -4,13 +4,13 @@ FirebaseFirestore db = FirebaseFirestore.instance;
 
 Future<List> getPeople() async {
   List people = [];
-  CollectionReference collectionReferencePeople = db.collection('people')
+  CollectionReference collectionReferencePeople = db.collection('people');
 
   QuerySnapshot queryPeople = await collectionReferencePeople.get();
   queryPeople.docs.forEach((documento) {
     people.add(documento.data());
-  })
-
+  });
+  await Future.delayed(const Duration(seconds: 5));
   return people;
 }
 
